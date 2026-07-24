41d4c43594d9a04340111e602261dcac2c1f3382
taposroy306919-collab <taposroy306919@gmail.com>
2026-07-24 11:15:37 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+
+import 'package:ecommerce/splash.dart';
+import 'package:flutter/material.dart';
+import 'login.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+
+      ),
+    );
+  }
+}
