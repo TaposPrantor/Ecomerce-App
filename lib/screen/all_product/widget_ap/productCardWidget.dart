8e1c2b0a3ea617fc7bdefd1afbd0252a8b523cad
taposroy306919-collab <taposroy306919@gmail.com>
2026-07-31 08:16:03 +0600

    Product Icon Clickable

--- lib/screen/all_product/widget_ap/productCardWidget.dart
+++ lib/screen/all_product/widget_ap/productCardWidget.dart
@@ -4,9 +4,7 @@ import '../../../custom_Widget/text.dart';
 import '../../../utilities/colors.dart';
 
 class ProductCardWidget extends StatelessWidget {
-  const ProductCardWidget({
-    super.key,
-  });
+  const ProductCardWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -20,18 +18,23 @@ class ProductCardWidget extends StatelessWidget {
                 height: 180,
                 width: double.infinity,
                 decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(14),
-                    image: DecorationImage(
-                        fit: BoxFit.fill,
-                        image: NetworkImage("https://newworld.co.za/cdn/shop/files/a5.webp?v=1750595591")
-                    )
+                  borderRadius: BorderRadius.circular(14),
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: NetworkImage(
+                      "https://newworld.co.za/cdn/shop/files/a5.webp?v=1750595591",
+                    ),
+                  ),
                 ),
               ),
               Positioned(
                 top: 5,
-                  right: 5,
-                  child: Icon(Icons.favorite_border_outlined)
-              )
+                right: 5,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.favorite_border_outlined),
+                ),
+              ),
             ],
           ),
           Column(
@@ -40,9 +43,22 @@ class ProductCardWidget extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  CustomText(text: "৳1199", fWeight: FontWeight.bold, fSize: 14,),
-                  CustomText(text: "৳1599", fWeight: FontWeight.bold, tDeco: TextDecoration.lineThrough,),
-                  CustomText(text: "25%", fWeight: FontWeight.bold, fSize: 14, color: AppColor.primaryColor,),
+                  CustomText(
+                    text: "৳1199",
+                    fWeight: FontWeight.bold,
+                    fSize: 14,
+                  ),
+                  CustomText(
+                    text: "৳1599",
+                    fWeight: FontWeight.bold,
+                    tDeco: TextDecoration.lineThrough,
+                  ),
+                  CustomText(
+                    text: "25%",
+                    fWeight: FontWeight.bold,
+                    fSize: 14,
+                    color: AppColor.primaryColor,
+                  ),
                 ],
               ),
             ],
@@ -50,12 +66,11 @@ class ProductCardWidget extends StatelessWidget {
           Row(
             spacing: 19,
             children: [
-              Icon(Icons.star, color: Colors.deepOrange,size: 18,),
-              CustomText(text: "4.6", color: AppColor.primaryColor,fSize: 16,),
-              CustomText(text: "(256)", color: Colors.black38,fSize: 16,),
+              Icon(Icons.star, color: Colors.deepOrange, size: 18),
+              CustomText(text: "4.6", color: AppColor.primaryColor, fSize: 16),
+              CustomText(text: "(256)", color: Colors.black38, fSize: 16),
             ],
-          )
-
+          ),
         ],
       ),
     );
