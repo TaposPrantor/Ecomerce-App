5f9709200e771286f3fc9dbcfd6def452b00230c
taposroy306919-collab <taposroy306919@gmail.com>
2026-07-31 09:01:55 +0600

    Slider Customize

--- lib/screen/details/product_details.dart
+++ lib/screen/details/product_details.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:ecommerce/custom_Widget/custom_cart.dart';
 import 'package:ecommerce/custom_Widget/text.dart';
+import 'package:ecommerce/screen/details/widget/imageSlider.dart';
 import 'package:flutter/material.dart';
 
 class ProductDetails extends StatefulWidget {
@@ -17,48 +18,11 @@ class _ProductDetailsState extends State<ProductDetails> {
       appBar: AppBar(actions: [CustomCart(), SizedBox(width: 10)]),
       body: Column(
         children: [
-          CarouselSlider(
-            options: CarouselOptions(
-              height: 250.0,
-              autoPlay: true,
-              viewportFraction: .9,
-              autoPlayInterval: Duration(seconds: 2),
-            ),
-            items: [1, 2, 3, 4, 5].map((i) {
-              return Builder(
-                builder: (BuildContext context) {
-                  return Stack(
-                    children: [
-                      Container(
-                        alignment: Alignment.center,
-                        width: MediaQuery.of(context).size.width,
-                        margin: EdgeInsets.symmetric(horizontal: 5.0),
-                        decoration: BoxDecoration(
-                          color: Colors.amber,
-                          borderRadius: BorderRadius.circular(10),
-                          image: DecorationImage(
-                            fit: BoxFit.cover,
-                              image: NetworkImage(
-                                "https://static.vecteezy.com/system/resources/thumbnails/066/223/245/small/professional-digital-camera-capturing-stunning-nature-shots-on-a-tripod-photo.jpg",
-                              )
-                          )
-                        ),
-                      ),
-                      Card(
-                        color: Colors.deepOrange,
-                        child: Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 5),
-                            child: CustomText(text: "40% OFF")
-                        )
-                      )
-                    ],
-                  );
-                },
-              );
-            }).toList(),
-          ),
+          ProductImageSlider(),
         ],
       ),
     );
   }
 }
+
+
---
+++ lib/screen/details/widget/imageSlider.dart
@@ -0,0 +1,54 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+import '../../../custom_Widget/text.dart';
+
+class ProductImageSlider extends StatelessWidget {
+  const ProductImageSlider({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: 250.0,
+        autoPlay: true,
+        viewportFraction: .9,
+        autoPlayInterval: Duration(seconds: 2),
+      ),
+      items: [1, 2, 3, 4, 5].map((i) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Stack(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.symmetric(horizontal: 5.0),
+                  decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                            "https://static.vecteezy.com/system/resources/thumbnails/066/223/245/small/professional-digital-camera-capturing-stunning-nature-shots-on-a-tripod-photo.jpg",
+                          )
+                      )
+                  ),
+                ),
+                Card(
+                    color: Colors.deepOrange,
+                    child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+                        child: CustomText(text: "40% OFF")
+                    )
+                )
+              ],
+            );
+          },
+        );
+      }).toList(),
+    );
+  }
+}
