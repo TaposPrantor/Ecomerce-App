d6e4cf63aee2518cecbc1647b2d52fb0d85ef751
taposroy306919-collab <taposroy306919@gmail.com>
2026-08-03 21:47:27 +0600

    MyOrder Page 1

---
+++ lib/screen 2/my_order/Custom_Widget/product_Order.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+import '../../../custom_Widget/CustomImage.dart';
+import '../../../custom_Widget/text.dart';
+import '../../../utilities/colors.dart';
+import 'CustomTextAdd.dart';
+
+class ProductOrderCard2 extends StatelessWidget {
+  final String orderId;
+  final String orderDate;
+  final String status;
+  final int totalItems;
+  final String totalPrice;
+  final String paymentMethod;
+  final List<String> imageUrls;
+  final VoidCallback? onViewDetails;
+  final VoidCallback? onStatusTap;
+
+  const ProductOrderCard2({
+    super.key,
+    required this.orderId,
+    required this.orderDate,
+    required this.status,
+    required this.totalItems,
+    required this.totalPrice,
+    required this.paymentMethod,
+    required this.imageUrls,
+    this.onViewDetails,
+    this.onStatusTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        width: double.infinity,
+        child: Column(
+          children: [
+
+            /// Order Info
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Order Id"),
+                    CustomText(
+                      text: orderId,
+                      fWeight: FontWeight.bold,
+                    ),
+                  ],
+                ),
+
+                Text(orderDate),
+
+                InkWell(
+                  onTap: onStatusTap,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 12,
+                      vertical: 4,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: CustomText(
+                      text: status,
+                      color: AppColor.primaryColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 15),
+
+            /// Item & Price
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CustomText(text: "$totalItems Items"),
+                CustomText(
+                  text: totalPrice,
+                  fWeight: FontWeight.bold,
+                  fSize: 16,
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 15),
+
+            /// Images
+            Row(
+              children: [
+                ...imageUrls.take(4).map(
+                      (e) => Padding(
+                    padding: const EdgeInsets.only(right: 5),
+                    child: CustomImage(imgUrl: e),
+                  ),
+                ),
+
+                if (imageUrls.length > 4)
+                  CustomAddTextCard(
+                    text: "+${imageUrls.length - 4}",
+                  ),
+              ],
+            ),
+
+            const SizedBox(height: 15),
+
+            /// Payment
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Payment Method"),
+                    CustomText(
+                      text: paymentMethod,
+                      fWeight: FontWeight.bold,
+                    ),
+                  ],
+                ),
+
+                InkWell(
+                  onTap: onViewDetails,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 12,
+                      vertical: 4,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: CustomText(
+                      text: "View Details",
+                      color: AppColor.primaryColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screen 2/my_order/my_order.dart
+++ lib/screen 2/my_order/my_order.dart
@@ -2,11 +2,11 @@
 import 'package:ecommerce/custom_Widget/custom_cart.dart';
 import 'package:ecommerce/custom_Widget/text.dart';
 import 'package:flutter/material.dart';
-
 import '../../custom_Widget/CustomImage.dart';
 import '../../utilities/colors.dart';
 import 'Custom_Widget/CustomTextAdd.dart';
 import 'Custom_Widget/productOrdderCard.dart';
+import 'Custom_Widget/product_Order.dart';
 
 
 class MyOrder extends StatefulWidget {
@@ -34,6 +34,19 @@ class _MyOrderState extends State<MyOrder> {
         body: Column(
           children: [
             ProductOrderCard(),
+            ProductOrderCard2(orderId: '#AB73H34',
+              orderDate: '03 Aug 2026',
+              status: 'Shipped',
+              totalItems: 2,
+              totalPrice: '৳2,399',
+              paymentMethod: 'Nagad',
+              imageUrls: [
+                "https://motionview.com.bd/_next/image?url=https%3A%2F%2Fmotionview.s3.amazonaws.com%2Fimages%2Fproducts%2Fprofile%2F177687341187.webp&w=3840&q=75",
+                "https://motionview.com.bd/_next/image?url=https%3A%2F%2Fmotionview.s3.amazonaws.com%2Fimages%2Fproducts%2Fprofile%2F177687341187.webp&w=3840&q=75",
+                "https://motionview.com.bd/_next/image?url=https%3A%2F%2Fmotionview.s3.amazonaws.com%2Fimages%2Fproducts%2Fprofile%2F177687341187.webp&w=3840&q=75",
+                "https://motionview.com.bd/_next/image?url=https%3A%2F%2Fmotionview.s3.amazonaws.com%2Fimages%2Fproducts%2Fprofile%2F177687341187.webp&w=3840&q=75",
+              ],
+            ),
           ],
         )
       ),
