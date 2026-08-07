edc52b7a5df96fb118849f18dbd563c5794a1c9a
taposroy306919-collab <taposroy306919@gmail.com>
2026-08-07 14:34:22 +0600

    My Cart 2.0

--- lib/screen 2/my_cart/my_cart.dart
+++ lib/screen 2/my_cart/my_cart.dart
@@ -1,9 +1,11 @@
 import 'dart:math';
 
+import 'package:ecommerce/custom_Widget/custom_button.dart';
 import 'package:flutter/material.dart';
 
 import '../../custom_Widget/custom_cart.dart';
 import '../../custom_Widget/text.dart';
+import '../../utilities/colors.dart';
 import 'Custom_Widget/MyCartContainer.dart';
 import 'Custom_Widget/MyCartShpping.dart';
 
@@ -50,6 +52,111 @@ class _MyCartState extends State<MyCart> {
             title: 'Jump-Iva Kade for Rainy Season',
             price: '3299',
           ),
+          Container(
+            height: 50,
+            //padding: EdgeInsets.symmetric(horizontal: 5),
+            decoration: BoxDecoration(
+              //borderRadius: BorderRadius.circular(5)
+            ),
+            child: Card(
+              child: Row(
+                spacing: 10,
+                children: [
+                  SizedBox(width: 12,),
+                  Icon(Icons.book),
+                  Text("Have a Coupon Code?"),
+                  Spacer(),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12,
+                        vertical: 4,
+                      ),
+                      decoration: BoxDecoration(
+                        //color: Colors.orange.shade100,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: CustomText(
+                        text: "Apply",
+                        color: Colors.deepOrange,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            height: 180,
+            padding: EdgeInsets.all(5),
+            child: Card(
+              child: Container(
+                padding: EdgeInsets.all(15),
+                child: Column(
+                  spacing: 10,
+                  children: [
+                    Row(
+                      children: [
+                        CustomText(text: "SubTotal(3 Item)", fWeight: FontWeight.bold,),
+                        Spacer(),
+                        CustomText(text: "৳ 3690"),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        CustomText(text: "Delivery Charge", fWeight: FontWeight.bold,),
+                        Spacer(),
+                        CustomText(text: "৳ 90"),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        CustomText(text: "Discount", fWeight: FontWeight.bold,),
+                        Spacer(),
+                        CustomText(text: "৳ 390"),
+                      ],
+                    ),
+                    Divider(),
+                    Row(
+                      children: [
+                        CustomText(text: "Total", fWeight: FontWeight.bold,),
+                        Spacer(),
+                        CustomText(text: "৳ 3390"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: Container(
+              height: 40,
+              width: 180,
+              decoration: BoxDecoration(
+                border: Border.all(color: AppColor.primaryColor, width: 3),
+                color: AppColor.primaryColor,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              alignment: Alignment.center,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomText(
+                    text: "Continue to Proceed",
+                    fWeight: FontWeight.bold,
+                    fSize: 14,
+                    color: Colors.white,
+                  ),
+                  SizedBox(width: 45,),
+                  Icon(Icons.arrow_right_alt_rounded , color: Colors.white,size: 35,)
+                ],
+              )
+            ),
+          ),
+          SizedBox(height: 20,)
         ],
       ),
     );
