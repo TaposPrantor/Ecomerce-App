ec86f8330aa498de94e99afa64f9a307b80927ee
taposroy306919-collab <taposroy306919@gmail.com>
2026-08-09 22:21:09 +0600

    Edit Profile 1

--- lib/screen 2/Edit_Profile/edit_profile.dart
+++ lib/screen 2/Edit_Profile/edit_profile.dart
@@ -13,68 +13,121 @@ class _EditProfileState extends State<EditProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: 150,
-            width: double.infinity,
-            padding: EdgeInsets.all(7),
-            decoration: BoxDecoration(
-              color: Colors.grey.shade100,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Row(
-              spacing: 15,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Stack(
-                      children: [
-                        CircleAvatar(
-                          radius: 60,
-                          backgroundImage: NetworkImage(
-                            "https://pbs.twimg.com/media/HFiAjgpaoAAyFNy.jpg",
-                          ),
-                        ),
-                        Positioned(
-                          bottom: -5,
-                          right: 5,
-                          child: Icon(Icons.camera_alt_rounded, size: 30,color: Colors.deepOrange,),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                Expanded(
-                  child: Column(
+      body: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: ListView(
+          children: [
+            Container(
+              height: 150,
+              width: double.infinity,
+              padding: EdgeInsets.all(7),
+              decoration: BoxDecoration(
+                color: Colors.grey.shade100,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                spacing: 15,
+                children: [
+                  Column(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    spacing: 10,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      CustomText(
-                        text: "MS Dhoni",
-                        fSize: 15,
-                        fWeight: FontWeight.bold,
-                      ),
-                      CustomText(
-                        text: "+92 01186324564",
-                        fSize: 12,
-                        fWeight: FontWeight.bold,
-                      ),
-                      CustomText(
-                        text: "msdhoni01icc@gmail.com",
-                        fSize: 10,
-                        fWeight: FontWeight.bold,
+                      Stack(
+                        children: [
+                          CircleAvatar(
+                            radius: 60,
+                            backgroundImage: NetworkImage(
+                              "https://pbs.twimg.com/media/HFiAjgpaoAAyFNy.jpg",
+                            ),
+                          ),
+                          Positioned(
+                            bottom: -5,
+                            right: 5,
+                            child: Icon(Icons.camera_alt_rounded, size: 30,color: Colors.deepOrange,),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ),
-              ],
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      spacing: 10,
+                      children: [
+                        CustomText(
+                          text: "MS Dhoni",
+                          fSize: 15,
+                          fWeight: FontWeight.bold,
+                        ),
+                        CustomText(
+                          text: "+92 01186324564",
+                          fSize: 12,
+                          fWeight: FontWeight.bold,
+                        ),
+                        CustomText(
+                          text: "msdhoni01icc@gmail.com",
+                          fSize: 10,
+                          fWeight: FontWeight.bold,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            Card(
+              child: Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 3,
+                  children: [
+                    CustomText(text: "Personal Information", fSize: 18, fWeight: FontWeight.bold,),
+                    CustomText(text: "Full Name", fSize: 10,),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: "MS Dhoni 7",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)
+                        )
+                      ),
+                    ),
+                    CustomText(text: "Phone Number", fSize: 10,),
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: "+123 94213764",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10)
+                          )
+                      ),
+                    ),
+                    CustomText(text: "Email Address", fSize: 10,),
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: "msd07ind@gmail.com",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10)
+                          )
+                      ),
+                    ),
+                    CustomText(text: "Date Of Birth", fSize: 10,),
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: "10 November, 2000",
+                          suffixIcon: Icon(Icons.edit_note),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10)
+                          )
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
